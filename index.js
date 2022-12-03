e54490ea9041a233030ca0bcd4a2824f723f0689
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2022-12-03 16:19:31 -0500

    test

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits, AuditLogEvent } = require('discord.js');
-const { token } = require('./config.json');
+// const { token } = require('./config.json');
+const token = process.env.Token;
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds],
