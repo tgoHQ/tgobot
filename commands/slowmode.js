62383279285d438b7c1a051ab4fc64cc7b2ba6ec
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2022-12-04 22:26:02 -0500

    error

--- commands/slowmode.js
+++ commands/slowmode.js
@@ -1,4 +1,4 @@
-//jshint esversion:8
+//jshint esversion:10
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 
 module.exports = {
@@ -18,12 +18,19 @@ module.exports = {
 			.setDescription('Reason for the slowmode')
 			.setRequired(true))
     .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
+
 	async execute(interaction) {
+
     const channel = interaction.options.getChannel('channel');
     const reason = interaction.options.getString('reason');
 		const time = interaction.options.getInteger('time');
 
-    await channel.setRateLimitPerUser(time, reason)
-      .then(interaction.reply(`:stopwatch: Set slowmode in <#${channel.id}> to \`${time}s\` with reason \`${reason}\`.`));
+		try {
+			await channel.setRateLimitPerUser(time, reason)
+	      .then(interaction.reply(`:stopwatch: Set slowmode in <#${channel.id}> to \`${time}s\` with reason \`${reason}\`.`))
+				.catch(function(e) {interaction.reply(`:octagonal_sign: Error: ${inlineCode(e.message)}`)})
+		}
+		catch {}
+
 	},
 };
