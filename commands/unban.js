1286105e817e450fb2dbc93348492a7ea5523f00
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2022-12-04 21:55:42 -0500

    d

--- commands/unban.js
+++ commands/unban.js
@@ -35,7 +35,7 @@ module.exports = {
 						interaction,
 					});
 				})
-				.catch(function(e) {interaction.reply(e)})
+				.catch(function(e) {interaction.reply('Error' + e)})
 		}
 		catch {}
 
