678092facedb9691592e24cb5b580daa98da222e
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2022-12-04 20:58:16 -0500

    test

--- commands/unban.js
+++ commands/unban.js
@@ -25,21 +25,21 @@ module.exports = {
 
 		//check if the user is actually banned
 		interaction.guild.bans.fetch(targetUser)
-		.then( //the user is banned, unban them
-			await interaction.guild.bans.remove(targetUser, reason)
-				.then(function() {
-					interaction.reply(`:unlock: Unbanned ${targetUser.toString()} with reason ${inlineCode(reason)}.`);
-					modlog.create({
-						type: "Unban",
-						author,
-						reason,
-						targetUser,
-						interaction,
-					});
-
-				}
+			.then( //the user is banned, unban them
+				// await interaction.guild.bans.remove(targetUser, reason)
+				// 	.then(function() {
+				// 		interaction.reply(`:unlock: Unbanned ${targetUser.toString()} with reason ${inlineCode(reason)}.`);
+				// 		modlog.create({
+				// 			type: "Unban",
+				// 			author,
+				// 			reason,
+				// 			targetUser,
+				// 			interaction,
+				// 		});
+				//
+				// 	}
+				// )
 			)
-		)
-		.catch(interaction.reply("That user is not banned!"));
+			.catch(interaction.reply("That user is not banned!"));
 	},
 };
