ab95e89bdfac53fdaaa3e6ceeb3392492a388a76
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2022-12-04 22:17:01 -0500

    e

--- modules/error.js
+++ modules/error.js
@@ -3,7 +3,7 @@ const { Client, Collection, Events, GatewayIntentBits, AuditLogEvent, REST, Rout
 const { EmbedBuilder } = require('discord.js');
 
 function error(interaction, error) {
-  interaction.reply(`:octagonal_sign: Error: ${inlineCode(e.message)}`);
+  interaction.reply(`:octagonal_sign: Error: ${inlineCode(error.message)}`);
 }
 
 module.exports = {
