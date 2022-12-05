ab758ee95ea89905e18e3ba52ff83d7522381436
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2022-12-05 02:24:30 -0500

    try again

--- commands/warn.js
+++ commands/warn.js
@@ -22,7 +22,7 @@ module.exports = {
     const reason = interaction.options.getString('reason');
 		const author = interaction.user;
 
-    interaction.reply(`<:icons_warning:908958943466893323> Warned ${targetUser.toString()} with reason ${inlineCode(reason)}.`);
+    interaction.reply(`<:warn:1049224507598061628> Warned ${targetUser.toString()} with reason ${inlineCode(reason)}.`);
 		modlog.create({
 			type: "Warn",
 			author,
