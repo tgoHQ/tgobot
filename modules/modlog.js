7d49a5924f12b295cd2f4c16ce0afdb34a8e2858
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2022-12-04 23:40:52 -0500

    add bulk delete number to modlog message

--- modules/modlog.js
+++ modules/modlog.js
@@ -18,6 +18,8 @@ function create(raw) {
   if ('targetChannel' in raw) embed.addFields({name: "Target Channel", value: raw.targetChannel.toString()});
   if ('slowmodeTime' in raw) embed.addFields({name: "Slowmode Time", value: raw.slowmode});
   if ('duration' in raw) embed.addFields({name: "Duration", value: raw.duration});
+  if ('bulkDeleteNumber' in raw) embed.addFields({name: "Messages Deleted", value: raw.bulkDeleteNumber});
+
 
   modlogChannel.send({ embeds: [embed] });
 
