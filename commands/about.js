7fababfe64fd99398dabe26d4fd8971118d9724f
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2022-12-06 05:30:48 -0500

    send embd

--- commands/about.js
+++ commands/about.js
@@ -12,6 +12,6 @@ module.exports = {
         {name: "Open Source", value: "This bot is open source and its code is freely available on <:github:1049292058583638038> [Github](https://github.com/kevin8181/tgobot3) under the AGPL-3.0 license."},
         {name: "Icons", value: "The icons used by this bot are provided by [iconsdiscord](https://discord.com/invite/aPvvhefmt3)."}
       );
-		await interaction.reply(``);
+		await interaction.reply({ embeds: [embed] });
 	},
 };
