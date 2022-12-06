542f1e93be5bb2b673b2cf0a92c0a3dafac9fa23
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2022-12-06 18:48:39 -0500

    about

--- commands/about.js
+++ commands/about.js
@@ -10,8 +10,9 @@ module.exports = {
       .setColor("137c5a")
       .setTitle("tgobot3")
       .addFields(
-        {name: "<:github:1049292058583638038> Open Source", value: "This bot is open source and its code is freely available on [Github](https://github.com/kevin8181/tgobot3) under the AGPL-3.0 license."},
+        {name: "<:djs:1049834255498354689> discord.js", value: "Built on the discord.js library."},
         {name: "<:shine:1049289235548614697> Icons", value: "The icons used by this bot are provided by [iconsdiscord](https://discord.com/invite/aPvvhefmt3)."},
+        {name: "<:github:1049292058583638038> Open Source", value: "This bot is open source and its code is freely available on [Github](https://github.com/kevin8181/tgobot3) under the AGPL-3.0 license."},
         {name: "<:developer:1049293038729576468> Developer", value: "tgobot3 was built by <@247070105916276736>."}
       );
 		await interaction.reply({ embeds: [embed] });
