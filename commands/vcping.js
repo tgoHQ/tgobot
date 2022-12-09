7fd28b6af504bcbee6efd4790d8d422dae29b221
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2022-12-09 07:27:41 -0500

    test without force

--- commands/vcping.js
+++ commands/vcping.js
@@ -10,8 +10,8 @@ module.exports = {
 			.setDescription('Channel where the VC is happening.')
 			.setRequired(true)),
 	async execute(interaction) {
-    const cachedChannel = interaction.options.getChannel('channel');
-    cachedChannel.fetch(true);
+    // const cachedChannel = interaction.options.getChannel('channel');
+    // cachedChannel.fetch(true);
     const channel = interaction.options.getChannel('channel');
     const connected = channel.members.toJSON().length;
 
