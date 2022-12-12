e5b4221da6eea39d6572087cb315f3f0a7780502
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2022-12-12 16:37:35 -0500

    test

--- events/bulkDelete.js
+++ events/bulkDelete.js
@@ -4,6 +4,7 @@ const { Events } = require('discord.js');
 module.exports = {
 	name: Events.messageDeleteBulk,
 	execute(client) {
+		client.channels.fetch(1049128945095811143).send("saw bulk delete event");
 		console.log(`got event!`);
 	},
 };
