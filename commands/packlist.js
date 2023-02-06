0ec3a354a0f671f3f5670801fcd48e7c26a78d50
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2023-02-06 05:29:28 -0500

    rm packlist

--- commands/packlist.js
+++
@@ -1,7 +0,0 @@
-const { SlashCommandBuilder } = require("discord.js");
-const storage = require("../modules/storage");
-
-module.exports = {
-	data: new SlashCommandBuilder().setName("packlist").setDescription("."),
-	async execute(interaction) {},
-};
