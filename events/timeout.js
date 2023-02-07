ed8be1a6ea398d8e73366699e98f120f9f9c355d
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2023-02-06 22:48:49 -0500

    test

--- events/timeout.js
+++ events/timeout.js
@@ -5,7 +5,15 @@ module.exports = {
 	execute(client, guildMemberUpdate) {
 		console.log("guild member update event");
 
-		console.log(guildMemberUpdate.communicationDisabledUntilTimestamp);
+		console.log(
+			"timestamp",
+			guildMemberUpdate.communicationDisabledUntilTimestamp
+		);
+		const timeoutEndDate = new Date(
+			guildMemberUpdate.communicationDisabledUntilTimestamp
+		);
+		console.log("end date", timeoutEndDate);
+		console.log("difference", timeoutEndDate - new Date());
 		//if user isn't timed out, return
 		// if (!guildMemberUpdate.communication_disabled_until) {
 		// 	return;
