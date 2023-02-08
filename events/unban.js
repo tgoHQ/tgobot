618b89980bb16aed555a3686460082bbe4af329a
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2023-02-08 16:10:40 -0500

    unban

---
+++ events/unban.js
@@ -0,0 +1,23 @@
+const { Events } = require("discord.js");
+const ModLog = require("../modules/modlog");
+
+module.exports = {
+	name: Events.GuildBanRemove,
+	execute(client, { user }) {
+		console.log(user);
+
+		const modlog = new ModLog({
+			type: "Unban",
+			author: client.user,
+			reason: "",
+			targetUser: user,
+		});
+
+		console.log(process.env.GUILD_ID);
+
+		modlog.post({
+			guild: client.guilds.cache.get(process.env.GUILD_ID),
+			client,
+		});
+	},
+};
