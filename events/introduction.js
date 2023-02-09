707535b43fa8ecbf70917fbd6a3bf1058692a8c6
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2023-02-09 13:36:26 -0500

    fix

--- events/introduction.js
+++ events/introduction.js
@@ -3,7 +3,7 @@ const { Events } = require("discord.js");
 module.exports = {
 	name: Events.MessageCreate,
 	execute(client, message) {
-		if (message.channel.id != proccess.env.INTRODUCTION_CHANNEL_ID) return;
+		if (message.channel.id != process.env.INTRODUCTION_CHANNEL_ID) return;
 		message.react("👋");
 	},
 };
