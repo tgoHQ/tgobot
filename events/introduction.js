a12c3b428027e7af54bfb7c9be00e09eeeb7b040
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2023-02-09 13:28:41 -0500

    test

--- events/introduction.js
+++ events/introduction.js
@@ -3,6 +3,7 @@ const { Events } = require("discord.js");
 module.exports = {
 	name: Events.MessageCreate,
 	execute(client, message) {
+		console.log("a");
 		if (message.channel !== 1073309027649454180) return;
 		message.react("👋");
 	},
