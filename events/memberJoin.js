3553712c63a3df524aebc708a7bfad8f87914b43
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2023-02-09 01:39:33 -0500

    test

--- events/memberJoin.js
+++ events/memberJoin.js
@@ -1,4 +1,5 @@
 const { Events, EmbedBuilder } = require("discord.js");
+const humanizeDuration = require("humanize-duration");
 
 module.exports = {
 	name: Events.GuildMemberAdd,
@@ -15,33 +16,33 @@ module.exports = {
 			.addFields({ name: "Username", value: member.user.tag });
 
 		permanentLogChannel.send({ embeds: [embed] });
-		console.log(timeSince(member.user.createdAt));
+		console.log(humanizeDuration(new Date() - member.user.createdAt));
 	},
 };
 
-function timeSince(date) {
-	const seconds = Math.floor((new Date() - date) / 1000);
+// function timeSince(date) {
+// 	const seconds = Math.floor((new Date() - date) / 1000);
 
-	let interval = Math.floor(seconds / 31536000);
+// 	let interval = Math.floor(seconds / 31536000);
 
-	if (interval > 1) {
-		return interval + " years";
-	}
-	interval = Math.floor(seconds / 2592000);
-	if (interval > 1) {
-		return interval + " months";
-	}
-	interval = Math.floor(seconds / 86400);
-	if (interval > 1) {
-		return interval + " days";
-	}
-	interval = Math.floor(seconds / 3600);
-	if (interval > 1) {
-		return interval + " hours";
-	}
-	interval = Math.floor(seconds / 60);
-	if (interval > 1) {
-		return interval + " minutes";
-	}
-	return Math.floor(seconds) + " seconds";
-}
+// 	if (interval > 1) {
+// 		return interval + " years";
+// 	}
+// 	interval = Math.floor(seconds / 2592000);
+// 	if (interval > 1) {
+// 		return interval + " months";
+// 	}
+// 	interval = Math.floor(seconds / 86400);
+// 	if (interval > 1) {
+// 		return interval + " days";
+// 	}
+// 	interval = Math.floor(seconds / 3600);
+// 	if (interval > 1) {
+// 		return interval + " hours";
+// 	}
+// 	interval = Math.floor(seconds / 60);
+// 	if (interval > 1) {
+// 		return interval + " minutes";
+// 	}
+// 	return Math.floor(seconds) + " seconds";
+// }
