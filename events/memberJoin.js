2b56fd2dc068b763893d21e1ca790a9049bb41d4
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2023-02-09 01:36:21 -0500

    a

--- events/memberJoin.js
+++ events/memberJoin.js
@@ -10,10 +10,38 @@ module.exports = {
 		const embed = new EmbedBuilder()
 			.setColor("137c5a")
 			.setTitle("User Joined")
-			.setImage(member.user.displayAvatarURL())
+			.setThumbnail(member.user.displayAvatarURL())
 			.setDescription(member.user.toString())
 			.addFields({ name: "Username", value: member.user.tag });
 
 		permanentLogChannel.send({ embeds: [embed] });
+		console.log(timeSince(member.user.createdAt));
 	},
 };
+
+function timeSince(date) {
+	const seconds = Math.floor((new Date() - date) / 1000);
+
+	let interval = Math.floor(seconds / 31536000);
+
+	if (interval > 1) {
+		return interval + " years";
+	}
+	interval = Math.floor(seconds / 2592000);
+	if (interval > 1) {
+		return interval + " months";
+	}
+	interval = Math.floor(seconds / 86400);
+	if (interval > 1) {
+		return interval + " days";
+	}
+	interval = Math.floor(seconds / 3600);
+	if (interval > 1) {
+		return interval + " hours";
+	}
+	interval = Math.floor(seconds / 60);
+	if (interval > 1) {
+		return interval + " minutes";
+	}
+	return Math.floor(seconds) + " seconds";
+}
