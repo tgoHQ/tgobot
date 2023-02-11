b23e212d57bc3380b17b06c5305e9f11672060cc
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2023-02-10 19:07:54 -0500

    a

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -6,10 +6,24 @@ module.exports = {
 		console.log("message deleted event");
 
 		if (message.guild?.id != process.env.GUILD_ID) {
-			console.log("not in guild");
+			console.log("message deleted from outside main guild");
 			return;
 		}
 
+		const temporaryLogChannel = message.guild.channels.cache.get(
+			process.env.TEMPORARY_LOG_CHANNEL_ID
+		);
+
 		console.log(message.content);
+		const embed = new EmbedBuilder()
+			.setColor("ff3131")
+			.setTitle("Message Deleted")
+			.setURL(message.url)
+			.setDescription(
+				`Message deleted by ${message.author} in ${message.channel}.`
+			)
+			.setFields({ name: "Content", value: message.content });
+
+		temporaryLogChannel.send({ embeds: [embed] });
 	},
 };
