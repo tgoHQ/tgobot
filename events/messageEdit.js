96edeb95c7023c37fb2d32b55f540d79282e92ab
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2023-02-10 19:38:31 -0500

    a

--- events/messageEdit.js
+++ events/messageEdit.js
@@ -1,33 +1,33 @@
-// //TODO make this work on older messages
+//TODO make this work on older messages
 
-// const { Events, EmbedBuilder } = require("discord.js");
+const { Events, EmbedBuilder } = require("discord.js");
 
-// module.exports = {
-// 	name: Events.MessageUpdate,
-// 	execute(client, oldMessage, newMessage) {
-// 		console.log("message update event");
+module.exports = {
+	name: Events.MessageUpdate,
+	execute(client, oldMessage, newMessage) {
+		console.log("message update event");
 
-// 		if (newMessage.guild?.id != process.env.GUILD_ID) {
-// 			console.log("message updated from outside main guild");
-// 			return;
-// 		}
+		if (newMessage.guild?.id != process.env.GUILD_ID) {
+			console.log("message updated from outside main guild");
+			return;
+		}
 
-// 		const temporaryLogChannel = newMessage.guild.channels.cache.get(
-// 			process.env.TEMPORARY_LOG_CHANNEL_ID
-// 		);
+		const temporaryLogChannel = newMessage.guild.channels.cache.get(
+			process.env.TEMPORARY_LOG_CHANNEL_ID
+		);
 
-// 		const embed = new EmbedBuilder()
-// 			.setColor("4a78fc")
-// 			.setTitle("Message Edited")
-// 			.setURL(newMessage.url)
-// 			.setDescription(
-// 				`Message edited by ${newMessage.author} in ${newMessage.channel}.`
-// 			)
-// 			.setFields(
-// 				{ name: "Before", value: oldMessage.content },
-// 				{ name: "After", value: newMessage.content }
-// 			);
+		const embed = new EmbedBuilder()
+			.setColor("4a78fc")
+			.setTitle("Message Edited")
+			.setURL(newMessage.url)
+			.setDescription(
+				`Message edited by ${newMessage.author} in ${newMessage.channel}.`
+			)
+			.setFields(
+				{ name: "Before", value: oldMessage.content },
+				{ name: "After", value: newMessage.content }
+			);
 
-// 		temporaryLogChannel.send({ embeds: [embed] });
-// 	},
-// };
+		temporaryLogChannel.send({ embeds: [embed] });
+	},
+};
