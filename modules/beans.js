a8c0afd918b970fd5da27023437846f1c1906d2b
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2023-06-03 02:26:38 -0400

    f

--- modules/beans.js
+++ modules/beans.js
@@ -1,26 +1,27 @@
-module.exports = {
-	callDB: async function callDB(url, method, body) {
-		try {
-			const response = await fetch(`${process.env.NOCODB_URL}${url}`, {
-				headers: {
-					"xc-token": process.env.NOCODB_TOKEN,
-				},
-				method: method,
-				body: body,
-			});
+async function callDB(url, method, body) {
+	try {
+		const response = await fetch(`${process.env.NOCODB_URL}${url}`, {
+			headers: {
+				"xc-token": process.env.NOCODB_TOKEN,
+			},
+			method: method,
+			body: body,
+		});
 
-			if (!response.ok) {
-				console.error("db call returned bad status code");
-				console.log(await response.json());
-				return {};
-			}
-
-			return await response.json();
-		} catch (e) {
-			console.error("db call error");
-			console.error(e);
+		if (!response.ok) {
+			console.error("db call returned bad status code");
+			console.log(await response.json());
+			return {};
 		}
-	},
+
+		return await response.json();
+	} catch (e) {
+		console.error("db call error");
+		console.error(e);
+	}
+}
+
+module.exports = {
 	topBeans: async function topBeans() {
 		const data = await callDB("/beans?sort=-quantity", "GET", {});
 		console.log(data);
