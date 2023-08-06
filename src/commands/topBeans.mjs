0959ad47cfc36fa02d448a76742453184cdcf466
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2023-08-06 18:23:04 -0400

    convert to import

---
+++ src/commands/topBeans.mjs
@@ -0,0 +1,12 @@
+import { SlashCommandBuilder } from "discord.js";
+import callDB from "../modules/database.mjs";
+
+export default {
+	data: new SlashCommandBuilder()
+		.setName("topbeans")
+		.setDescription("Lists users with the most beans."),
+	async execute(interaction) {
+		const data = await callDB("/beans?sort=-quantity", "GET");
+		interaction.reply(JSON.stringify(data));
+	},
+};
