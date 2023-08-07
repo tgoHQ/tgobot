9001cbad6c6c8bf3e954594b64faf3b6ab0561e7
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2023-08-06 20:07:45 -0400

    .

--- src/commands/register.mjs
+++ src/commands/register.mjs
@@ -4,9 +4,9 @@ import fs from "node:fs";
 import path from "node:path";
 
 export default async function register() {
-	const clientId = process.env.DISCORD_CLIENT_ID;
-	const guildId = process.env.DISCORD_GUILD_ID;
-	const token = process.env.DISCORD_BOT_TOKEN;
+	const CLIENT_ID = process.env.DISCORD_CLIENT_ID;
+	const GUILD_ID = process.env.DISCORD_GUILD_ID;
+	const TOKEN = process.env.DISCORD_BOT_TOKEN;
 
 	const commands = [];
 	// Grab all the command folders from the commands directory
@@ -14,28 +14,30 @@ export default async function register() {
 	const commandFolders = fs.readdirSync(foldersPath);
 
 	for (const folder of commandFolders) {
-		// Grab all the command files from the commands directory
-		const commandsPath = path.join(foldersPath, folder);
-		const commandFiles = fs
-			.readdirSync(commandsPath)
-			.filter((file) => file.endsWith(".js"));
+		if (!folder.endsWith(".mjs")) {
+			// Grab all the command files from the commands directory
+			const commandsPath = path.join(foldersPath, folder);
+			const commandFiles = fs
+				.readdirSync(commandsPath)
+				.filter((file) => file.endsWith(".js"));
 
-		// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-		for (const file of commandFiles) {
-			const filePath = path.join(commandsPath, file);
-			const command = import(filePath);
-			if ("data" in command && "execute" in command) {
-				commands.push(command.data.toJSON());
-			} else {
-				console.log(
-					`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-				);
+			// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+			for (const file of commandFiles) {
+				const filePath = path.join(commandsPath, file);
+				const command = import(filePath);
+				if ("data" in command && "execute" in command) {
+					commands.push(command.data.toJSON());
+				} else {
+					console.log(
+						`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+					);
+				}
 			}
 		}
 	}
 
 	// Construct and prepare an instance of the REST module
-	const rest = new REST().setToken(token);
+	const rest = new REST().setToken(TOKEN);
 
 	// and deploy your commands!
 	(async () => {
@@ -46,7 +48,7 @@ export default async function register() {
 
 			// The put method is used to fully refresh all commands in the guild with the current set
 			const data = await rest.put(
-				Routes.applicationGuildCommands(clientId, guildId),
+				Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
 				{ body: commands }
 			);
 
