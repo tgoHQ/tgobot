777850a71467f1c204ca3b35b25c1dc367d67297
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2023-10-13 12:51:11 -0400

    test

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,37 +1,3 @@
-import fs from "node:fs";
-import path from "node:path";
+import about from "./general/about.js";
 
-async function getCommands() {
-	const commands = [];
-
-	const foldersPath = path.resolve("src/commands");
-
-	const folderNameRegex = /^[^.]+$/;
-	const commandFolders = fs
-		.readdirSync(foldersPath)
-		.filter((file) => folderNameRegex.test(file));
-
-	for (const folder of commandFolders) {
-		// Grab all the command files from the commands directory
-		const commandsPath = path.join(foldersPath, folder);
-		const commandFiles = fs
-			.readdirSync(commandsPath)
-			.filter((file) => file.endsWith(".js"));
-
-		for (const file of commandFiles) {
-			const filePath = path.join(commandsPath, file);
-			const command = (await import(filePath)).default;
-
-			if (command?.data && typeof command?.execute === "function") {
-				commands.push(command);
-			} else {
-				console.log(
-					`[WARNING] The command at ${filePath} is missing a required "data" or "execute" export.`
-				);
-			}
-		}
-	}
-	return commands;
-}
-
-export default await getCommands();
+export default [about];
