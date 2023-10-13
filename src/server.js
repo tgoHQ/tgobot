e09d6dbee0b6d206842917e94e58e5b94ff691b4
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2023-10-13 12:52:55 -0400

    .

--- src/server.js
+++ src/server.js
@@ -25,26 +25,26 @@ const client = new Client({
 
 import commands from "./commands/index.js";
 
-import registerSlashCommands from "./commands/register.js";
-await registerSlashCommands(commands);
+// import registerSlashCommands from "./commands/register.js";
+// await registerSlashCommands(commands);
 
-import useSlashCommands from "./commands/use.js";
-await useSlashCommands(client, commands);
+// import useSlashCommands from "./commands/use.js";
+// await useSlashCommands(client, commands);
 
-//load events
-const eventsPath = path.resolve("src/events");
-const eventFiles = fs
-	.readdirSync(eventsPath)
-	.filter((file) => file.endsWith(".mjs"));
-for (const file of eventFiles) {
-	const filePath = path.join(eventsPath, file);
-	const event = (await import(filePath)).default;
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(client, ...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(client, ...args));
-	}
-}
-console.log(`Client listening for ${eventFiles.length} events.`);
+// //load events
+// const eventsPath = path.resolve("src/events");
+// const eventFiles = fs
+// 	.readdirSync(eventsPath)
+// 	.filter((file) => file.endsWith(".mjs"));
+// for (const file of eventFiles) {
+// 	const filePath = path.join(eventsPath, file);
+// 	const event = (await import(filePath)).default;
+// 	if (event.once) {
+// 		client.once(event.name, (...args) => event.execute(client, ...args));
+// 	} else {
+// 		client.on(event.name, (...args) => event.execute(client, ...args));
+// 	}
+// }
+// console.log(`Client listening for ${eventFiles.length} events.`);
 
 client.login(process.env.TOKEN);
