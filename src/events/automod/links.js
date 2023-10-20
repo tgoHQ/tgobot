893f076bd668fb1ec99b5ed13c6371523822f6d8
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2023-10-20 17:37:54 -0400

    .

---
+++ src/events/automod/links.js
@@ -0,0 +1,37 @@
+import { Events, Message, Client } from "discord.js";
+
+export default {
+	name: Events.MessageCreate,
+	/**
+	 *
+	 * @param {Client} client
+	 * @param {Message} message
+	 */
+	async execute(client, message) {
+		const member = await message.guild.members.fetch(message.author);
+
+		//if member has verified role, ignore
+		if (
+			!member.roles.cache.some(
+				(role) => role.id === process.env.INTRODUCED_ROLE_ID
+			)
+		) {
+			console.log("user is verified");
+			return;
+		}
+
+		//if member joined more than 3 hours ago, ignore
+		if (member.joinedTimestamp < Date.now - 3 * 60 * 60 * 1000) {
+			console.log("user joined over 3 hours ago");
+			return;
+		}
+	},
+};
+
+//on message send
+
+//if message contains link AND user is not Introduced AND user joined less than 3 hours ago
+
+//delete message
+//notify author
+//send message in alerts channel
