94f7aea273ee65995ffda107da6f05d9fd3903bd
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2023-10-20 00:36:51 -0400

    .

--- src/commands/general/grade.js
+++ src/commands/general/grade.js
@@ -4,9 +4,17 @@ import { GradeScales } from "@openbeta/sandbag";
 export default {
 	data: new SlashCommandBuilder()
 		.setName("grades")
-		.setDescription("Displays information about a climbing grade."),
+		.setDescription("Displays information about a climbing grade.")
+		.addStringOption((option) =>
+			option
+				.setName("grade")
+				.setDescription("The grade to look up.")
+				.setRequired(true)
+		),
 
 	async execute(interaction) {
-		await interaction.reply("yo");
+		const input = interaction.options.getString("grade");
+
+		await interaction.reply(input);
 	},
 };
