030bd002625914f05eb1a131317ec1e39c9fccf7
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2023-10-20 17:45:53 -0400

    .

--- src/events/automod/links.js
+++ src/events/automod/links.js
@@ -8,6 +8,14 @@ export default {
 	 * @param {Message} message
 	 */
 	async execute(client, message) {
+		const linkRE = /https?:\/\/[^.]*\./;
+
+		//if message does not contain a link, ignore
+		if (!linkRE.test(message.content)) {
+			console.log("message contains no links");
+			return;
+		}
+
 		const member = await message.guild.members.fetch(message.author);
 
 		//if member has verified role, ignore
