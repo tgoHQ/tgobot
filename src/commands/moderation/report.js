dbe33d6a77113fb59c2e58964a92b88bcd5f3c28
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2023-10-20 19:41:14 -0400

    .

---
+++ src/commands/moderation/report.js
@@ -0,0 +1,29 @@
+import {
+	SlashCommandBuilder,
+	PermissionFlagsBits,
+	EmbedBuilder,
+} from "discord.js";
+
+export default {
+	data: new SlashCommandBuilder()
+		.setName("report")
+		.setDescription("Displays moderation info about a user.")
+		.addUserOption((option) =>
+			option
+				.setName("user")
+				.setDescription("The user to inspect")
+				.setRequired(true)
+		)
+		.setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
+
+	async execute(interaction) {
+		const member = interaction.options.getMember("user");
+		const targetUser = member.user;
+
+		const embed = new EmbedBuilder()
+			.setColor("137c5a")
+			.setTitle(member.user.toString());
+
+		interaction.reply({ embeds: [embed] });
+	},
+};
