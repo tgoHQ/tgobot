b3e601ba8c538405be460ee96f017793ed50ad5f
kevin8181 <66894759+kevin8181@users>
2024-04-05 00:40:39 -0400

    .

--- src/events/logging/ban.ts
+++ src/events/logging/ban.ts
@@ -1,30 +1,30 @@
 import { Events } from "discord.js";
 import type { Event } from "../index.js";
-import { AuditLogEvent } from "discord.js";
-import client from "../../lib/client.js";
-import ban from "../../lib/moderation/users/actions/ban.js";
+// import { AuditLogEvent } from "discord.js";
+// import client from "../../lib/client.js";
+// import ban from "../../lib/moderation/users/actions/ban.js";
 
 export default {
 	name: Events.GuildAuditLogEntryCreate,
-	async execute(auditLog) {
-		console.log(0);
-		if (auditLog.action !== AuditLogEvent.MemberBanAdd) return;
-		console.log(1);
-		if (!auditLog.targetId) throw new Error("No target found");
-
-		//if no author found, use bot
-		const author = await client.users.fetch(
-			auditLog.executorId ?? client.user!.id
-		);
-
-		const target = await client.users.fetch(auditLog.targetId);
-
-		ban({
-			user: target,
-			reason: auditLog.reason ?? "",
-			author,
-			execute: false,
-		});
+	async execute() {
+		console.log(9);
+		// //if this isn't a ban, ignore
+		// if (auditLog.action !== AuditLogEvent.MemberBanAdd) return;
+		// console.log(1);
+		// //get target
+		// if (!auditLog.targetId) return;
+		// const target = await client.users.fetch(auditLog.targetId);
+		// //get author. if no author use bot
+		// const author = await client.users.fetch(
+		// 	auditLog.executorId ?? client.user!.id
+		// );
+		// await ban({
+		// 	user: target,
+		// 	reason: auditLog.reason ?? "",
+		// 	author,
+		// 	execute: false,
+		// });
+		// return;
 	},
 } satisfies Event<Events.GuildAuditLogEntryCreate>;
 
