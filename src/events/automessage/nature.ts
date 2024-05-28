3b9b280154c562236c658c21cb8a54ae6ce62152
kevin8181 <66894759+kevin8181@users>
2024-05-28 13:09:41 -0400

    3 photos trigger

--- src/events/automessage/nature.ts
+++ src/events/automessage/nature.ts
@@ -16,7 +16,7 @@ export default {
 	async execute(message) {
 		if (message.channel !== CHANNEL_NATURE) return; //if message not from photos channel, return
 
-		if (message?.attachments.size >= 2) {
+		if (message?.attachments.size >= 3) {
 			await message.reply(`
 				Thanks for posting! This channel is intended mainly for text discussion about nature. Please use ${CHANNEL_PHOTOS} instead for posting nature photography.
 			`);
