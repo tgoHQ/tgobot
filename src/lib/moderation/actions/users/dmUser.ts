9896ea7adb6d3601e782c9b682de978278319ace
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2025-11-30 03:32:43 -0500

    improve dm format

--- src/lib/moderation/actions/users/dmUser.ts
+++ src/lib/moderation/actions/users/dmUser.ts
@@ -1,5 +1,5 @@
 import type { User } from "discord.js";
-import { removeTabs } from "../../../../util/removeTabs";
+import { removeTabs } from "../../../../util/removeTabs.js";
 
 export async function modActionDmUser({
 	targetUser,
@@ -16,15 +16,14 @@ export async function modActionDmUser({
 		await targetUser.send({
 			content: removeTabs(`
 				You have recieved a moderation action on The Great Outdoors.
-
 				### Action
 				${string}
 				### Reason
 				${reason ?? "No reason provided."}
 				### Author
-				${author.displayName}
+				${author}
 
-				You can review our [moderation policies](https://docs.google.com/document/d/1q-EzLayHiIS-cDfmuWoJwnuKGfVc7d2sjX7_LEIuTMA/) or submit an [appeal](https://forms.gle/4jWKXXXjWPhp9GbW6).
+				You can review our [moderation policies](https://docs.google.com/document/d/1q-EzLayHiIS-cDfmuWoJwnuKGfVc7d2sjX7_LEIuTMA/) or [submit an appeal](https://forms.gle/4jWKXXXjWPhp9GbW6).
 			`),
 		});
 	} catch {}
